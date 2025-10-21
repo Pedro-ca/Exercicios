7d9a55306953830993d6aaa2df54560bbdb30fab
Pedro-ca <pedro.decastrom@gmail.com>
2025-10-21 16:25:56 -0300

    Create exercicio de repetição #13.py

---
+++ exercicio de repetição #13.py
@@ -0,0 +1,7 @@
+base = int(input('me dê uma base '))
+expoente = int(input('me dê um expoente '))
+
+potencia = base ** expoente
+
+print(f'a potencia de base {base} com expoente de {expoente} é')
+print(potencia)
