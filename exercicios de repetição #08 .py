eebbed439b3c246fc66f6d0a57e2a4233b6ef35d
Pedro-ca <pedro.decastrom@gmail.com>
2025-10-21 15:46:04 -0300

    Create exercicios de repetição #08 .py

---
+++ exercicios de repetição #08 .py
@@ -0,0 +1,9 @@
+num_01 = int(input('digite 5 numeros '))
+num_02 = int(input())
+num_03 = int(input())
+num_04 = int(input())
+num_05 = int(input())
+
+media = num_01 + num_02 + num_03 + num_04 + num_05 / 5
+
+print(f'a media dos numeros é {media}')
