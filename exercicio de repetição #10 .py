8511ed33494ba072e692cdd538b17bcbeced37fb
Pedro-ca <pedro.decastrom@gmail.com>
2025-10-21 15:52:56 -0300

    Create exercicio de repetição #10 .py

---
+++ exercicio de repetição #10 .py
@@ -0,0 +1,10 @@
+num_01 = int(input('digite 2 numeros '))
+num_02 = int(input())
+
+spc = num_01 - num_02
+
+if spc < 0:
+    print(f'o espaço entre os numeros é {spc * -1}')
+
+else:
+    print(f'o espaço entre os numeros é {spc}')
