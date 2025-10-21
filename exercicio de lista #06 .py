6a404c3902ddb1644f4692521eb0bdfabfc4741d
Pedro-ca <pedro.decastrom@gmail.com>
2025-10-21 14:19:20 -0300

    Create exercicio de lista #06 .py

---
+++ exercicio de lista #06 .py
@@ -0,0 +1,16 @@
+nota_01 = int(input('me dê a nota de Fulano '))
+nota_02 = int(input('me dê a nota de ciclano '))
+nota_03 = int(input('me dê a nota de Fulana '))
+nota_04 = int(input('me dê a nota de Alano '))
+
+if nota_01 >= 7:
+    print('Fulano aprovado')
+
+if nota_02 >= 7:
+    print('Ciclano aprovado')
+
+if nota_03 >= 7:
+    print('Fulana aprovado')
+
+if nota_04 >= 7:
+    print('Alano aprovado')
