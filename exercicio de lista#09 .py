7ac6c8d59022fa82b077045f82a88d27bb3ff9f2
Pedro-ca <pedro.decastrom@gmail.com>
2025-10-22 15:22:00 -0300

    Create exercicio de lista#09 .py

---
+++ exercicio de lista#09 .py
@@ -0,0 +1,7 @@
+num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+num2 = [1, 4, 9, 16, 25, 36, 64, 81, 100]
+
+resultado = sum(num + num2)
+
+print(num)
+print(f'o resultado das soma dos quadrados é {resultado}')
