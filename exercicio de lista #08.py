399d49185d6472f21cbe858f77dea0ca5a7025c3
Pedro-ca <pedro.decastrom@gmail.com>
2025-10-22 15:13:15 -0300

    Create exercicio de lista #08.py

---
+++ exercicio de lista #08.py
@@ -0,0 +1,24 @@
+altura1 = float(input('me dê sua altura em metros '))
+idade1 = int(input('me dê sua idade '))
+
+altura2 = float(input('me dê sua altura em metros '))
+idade2 = int(input('me dê sua idade '))
+
+altura3 = float(input('me dê sua altura em metros '))
+idade3 = int(input('me dê sua idade '))
+
+pessoa01 = ['metros', altura1, 'anos', idade1, ]
+pessoa01.reverse()
+
+pessoa02 = ['metros', altura2, 'anos', idade2 ]
+pessoa02.reverse()
+
+pessoa03 = ['metros', altura3, 'anos', idade3, ]
+pessoa03.reverse()
+
+print('você tem respectivamente')
+print(pessoa01)
+print()
+print(pessoa02)
+print()
+print(pessoa03)
